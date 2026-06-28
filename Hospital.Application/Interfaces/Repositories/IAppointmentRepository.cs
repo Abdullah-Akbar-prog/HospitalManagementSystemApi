079ad659fff400762118375dfd6c43ae536977c2
Abdullah-Akbar-prog <mabdullahstu@gmail.com>
2026-06-28 21:01:37 +0500

    update IAppointmentRepository

--- Hospital.Application/Interfaces/Repositories/IAppointmentRepository.cs
+++ Hospital.Application/Interfaces/Repositories/IAppointmentRepository.cs
@@ -5,8 +5,9 @@ namespace Hospital.Application.Interfaces.Repositories
     public interface IAppointmentRepository : IRepository<Appointment>
     {
         Task<Appointment?> GetByDoctorAndTime(int doctorId, DateTime appointmentDate);
-        Task<Appointment?> GetByPatientIdAsync(int doctorId, DateTime appointmentDate);
-        Task<Appointment?> GetByDoctorIdAsync(int doctorId, DateTime appointmentDate);
+        Task<Appointment?> GetByPatientIdAsync(int patientId);
+        Task<Appointment?> GetByDoctorIdAsync(int doctorId);
+        Task<int?> CountTodayAsync();
 
     }
 }
